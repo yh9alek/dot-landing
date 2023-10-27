7dc08745d773a52570a37741de0892573e6e044c
Yohan Alek <yh9dev@gmail.com>
2023-10-26 21:16:30 -0700

    The populars section is finished

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-// Swiper JS
+// Swiper JS to work in products section
 var swiper = new Swiper(".products-swiper", {
     slidesPerView: 1,
     spaceBetween: 30,
@@ -28,3 +28,36 @@ var swiper = new Swiper(".products-swiper", {
       },
     },
   });
+
+  // Swiper JS to work in populars section
+var swiper = new Swiper(".populars-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  centeredSlides: false,
+  loop: true,
+  /*autoplay: {
+    delay: 7000,
+    disableOnInteraction: false,
+  },*/
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next2",
+  },
+  breakpoints: {
+    564: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    835: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1106: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+});
