f1a2866d47bd2c45be1977f01c4a1824ca72442d
Yohan Alek <yh9dev@gmail.com>
2023-10-28 20:02:01 -0700

    Se programó correctamente la autenticación para impedir el acceso a la página de admin.html sin logeo

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,4 +1,32 @@
 import { agregarProducto } from '../js/add.js';
+import { auth } from '../app/firebase.js';
+import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js';
+import { signOut } from 'https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js';
+
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    // El usuario no está autenticado, redirigir a la página de inicio de sesión
+    window.location.href = 'login.html';
+  }
+});
+
+const back = document.querySelector('.header__link');
+
+back.addEventListener('click', e => {
+	cerrarSesion();
+});
+
+const cerrarSesion = () => {
+	// Cerrar sesión
+	signOut(auth).then(() => {
+		// La sesión ha sido cerrada con éxito
+		console.log('Sesión cerrada exitosamente');
+		document.location.assign('../../index.html');
+	}).catch((error) => {
+		// Hubo un error al cerrar sesión
+		console.error('Error al cerrar sesión:', error);
+	});
+}
 
 const btn__add = document.querySelector('.btn__add');
 
