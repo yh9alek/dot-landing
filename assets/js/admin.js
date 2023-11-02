7c0129330b99deaac82521e93395073f09ee83c6
Yohan Alek <yh9dev@gmail.com>
2023-11-01 21:33:44 -0700

    Se terminó de programar todas las operaciones con firebase (añadir, actualizar, eliminar) tanto para productos comunes como para productos populares

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,7 +1,6 @@
 import { addupt } from './add.js';
-import { eliminarProducto } from './delete.js';
 import { adduptpo } from './add.js';
-import { eliminarPopular } from './delete.js';
+import { eliminar } from './delete.js';
 import { auth } from '../app/firebase.js';
 import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js';
 import { signOut } from 'https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js';
@@ -61,7 +60,7 @@ btn__add.addEventListener('click', async (e) => {
 
 btn__rem.addEventListener('click', async (e) => {
   e.preventDefault();
-  await eliminarProducto(codrempr.value);
+  await eliminar(codrempr.value, 'productos');
 });
 
 const codigo_po = document.querySelector('.populares #cod');
@@ -70,6 +69,8 @@ const precio_po = document.querySelector('.populares #precio');
 const rate_po = document.querySelector('#rate');
 const sale_po = document.querySelector('#sale');
 const img_po = document.querySelector('.populares #file');
+
+const codrempo = document.querySelector('#cod_rempo');
 
 btn__addp.addEventListener('click', async (e) => {
   e.preventDefault();
@@ -83,5 +84,5 @@ btn__addp.addEventListener('click', async (e) => {
 
 btn__remp.addEventListener('click', async (e) => {
   e.preventDefault();
-  await eliminarPopular(document.querySelector('.populares .remove input[type="text"]').value);
+  await eliminar(codrempo.value, 'populares');
 });
--- assets/js/delete.js
+++ assets/js/delete.js
@@ -2,10 +2,10 @@ import { doc, deleteDoc, query, where, getDocs, collection } from 'https://www.g
 
 import { db } from '../app/firebase.js';
 
-export const eliminarProducto = async (codigo) => {
+export const eliminar = async (codigo, coleccion) => {
   try {
     // Buscar el documento con el código proporcionado
-    const q = query(collection(db, 'productos'), where('codigo', '==', codigo));
+    const q = query(collection(db, coleccion), where('codigo', '==', codigo));
     const querySnapshot = await getDocs(q);
 
     // Verificar si se encontró algún documento con el código
@@ -16,19 +16,6 @@ export const eliminarProducto = async (codigo) => {
 
     // Eliminar el primer documento encontrado (asumiendo que los códigos son únicos)
     const productoDoc = querySnapshot.docs[0].ref;
-    await deleteDoc(productoDoc);
-
-    console.log('Documento eliminado correctamente');
-  } catch (error) {
-    console.error('Error al eliminar el documento:', error);
-  }
-};
-
-
-export const eliminarPopular = async (id) => {
-  try {
-    const productoDoc = doc(db, 'populares', id);
-
     await deleteDoc(productoDoc);
 
     console.log('Documento eliminado correctamente');
